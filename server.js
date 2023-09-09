f8a5e40fff81903e72e092c3edfc31239ff7c234
Yosr Naija <yosr.naija@live.com>
2023-09-09 09:40:25 +0100

    first server with only node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http")
-
+//createServer accepet un callback qui a comme paramètre la requete et la réponse
 const server = http.createServer((req, res) => {
   res.end("Voilà la réponse du serveur !")
 })
-
+// si pas de variable d'environnement PORT, on va écouter le port 5000
 server.listen(process.env.PORT || 5000)
