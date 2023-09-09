d8881dcee877ec5d38574909bc734d0c43713a6c
Yosr Naija <yosr.naija@live.com>
2023-09-09 09:59:24 +0100

    fix server

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.set("port", port)
 const server = http.createServer(app)
 server.on("listening", () => {
   const address = server.address()
-  console.log(address)
   const bind = typeof address === "string" ? "pipe " + address : "port " + port
   console.log("Listening on " + bind)
 })
