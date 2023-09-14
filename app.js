1501d19eef3ab2edd80fa758e2e58d7cae12e85e
Yosr Naija <yosr.naija@live.com>
2023-09-14 07:58:00 +0100

    get route with app.use

--- app.js
+++ app.js
@@ -6,21 +6,37 @@ const app = express()
 // })
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !")
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  )
   next()
 })
 
-app.use((req, res, next) => {
-  res.status(201)
-  next()
+app.use("/api/todos/", (req, res, next) => {
+  const todos = [
+    {
+      _id: "1",
+      title: "learn js",
+      duration: "30",
+    },
+    {
+      _id: "2",
+      title: "learn nodeJS",
+      duration: "40",
+    },
+    {
+      _id: "3",
+      title: "learn react",
+      duration: "60",
+    },
+  ]
+  res.status(200).json(todos)
 })
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" })
-  next()
-})
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !")
-})
 module.exports = app
