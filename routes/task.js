62025eb32340fee22cdafcb23fd26fb4cdc9e6fc
Yosr Naija <yosr.naija@live.com>
2023-11-14 11:27:05 +0100

    add isAdmin middleware

--- routes/task.js
+++ routes/task.js
@@ -9,7 +9,7 @@ const auth = require("../middleware/auth")
 /*remplacer use car car la logique GET interceptera 
  actuellement toutes les requêtes envoyées à votre endpoint 
  */
-router.get("/", auth.loggedMiddleware, taskController.fetchTasks)
+router.get("/", auth.loggedMiddleware, auth.isAdmin, taskController.fetchTasks)
 router.post("/", auth.loggedMiddleware, taskController.addTask)
 
 router.get("/:id", auth.loggedMiddleware, taskController.getTaskById)
