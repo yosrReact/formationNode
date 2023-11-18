89705e6a7a953d700f2dbe90e8634deb2e339bd1
Yosr Naija <yosr.naija@live.com>
2023-11-18 17:02:43 +0100

    add swagger

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const taskRoutes = require("./routes/task")
 const userRoutes = require("./routes/user")
-
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
 //?retryWrites=true&w=majority
 mongoose
   .connect(
@@ -13,11 +14,6 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"))
 const app = express()
 
-/*
-Avec ceci, Express prend toutes les requêtes qui ont comme Content-Type  application/json 
-et met à disposition leur  body  directement sur l'objet req, 
-ce qui nous permet d'écrire le middleware POST
-*/
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -32,6 +28,80 @@ app.use((req, res, next) => {
   )
   next()
 })
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Todos Express API with Swagger",
+      version: "0.1.0",
+      description: "This is a simple CRUD API application",
+
+      contact: {
+        name: "Yosr Naija",
+        url: "https://www.linkedin.com/in/yosrnaija/",
+        email: "yosr.naija@igmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5001/api",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+}
+
+//           components: {
+//       // securitySchemes: {
+//       //   bearerAuth: {
+//       //     type: "http",
+//       //     scheme: "bearer",
+//       //     bearerFormat: "JWT",
+//       //   },
+//       // },
+//       responses: {
+//         200: {
+//           description: "Success",
+//         },
+//         400: {
+//           description: "Bad request. You may need to verify your information.",
+//         },
+//         401: {
+//           description: "Unauthorized request, you need additional privileges",
+//         },
+//         403: {
+//           description:
+//             "Forbidden request, you must login first. See /auth/login",
+//         },
+//         404: {
+//           description: "Object not found",
+//         },
+//         422: {
+//           description:
+//             "Unprocessable entry error, the request is valid but the server refused to process it",
+//         },
+//         500: {
+//           description: "Unexpected error, maybe try again later",
+//         },
+//       },
+//     },
+//     // security: [
+//     //   {
+//     //     bearerAuth: [],
+//     //   },
+//     // ],
+//   },
+// }
+
+const specs = swaggerJsdoc(options)
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+)
 
 app.use("/api/tasks/", taskRoutes)
 app.use("/api/auth/", userRoutes)
--- routes/task.js
+++ routes/task.js
@@ -1,3 +1,9 @@
+//*       401:
+// *         $ref: '#/components/responses/401'
+
+// *                  items:
+//*                      $ref: '#/components/schemas/Task'
+
 const express = require("express")
 
 const router = express.Router()
@@ -5,13 +11,188 @@ const router = express.Router()
 const taskController = require("../controllers/task")
 
 const auth = require("../middleware/auth")
-
-/*remplacer use car car la logique GET interceptera 
- actuellement toutes les requêtes envoyées à votre endpoint 
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: The Tasks managing API
  */
-router.get("/", auth.loggedMiddleware, auth.isAdmin, taskController.fetchTasks)
-router.post("/", auth.loggedMiddleware, taskController.addTask)
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     newTask:
+ *       type: object
+ *       required:
+ *         - title
+ *         - duration
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The task title
+ *         duration:
+ *           type: string
+ *           description: The task duration
+ *         description:
+ *           type: string
+ *           description: The task description
+ *       example:
+ *         title: learn react
+ *         duration: 130
+ *         description: learn the fundamentals of react
+ *     Task:
+ *          allOf:
+ *              - type: object
+ *                properties:
+ *                  _id:
+ *                      type: string
+ *                      description: The auto-generated id of the task
+ *              - $ref: '#/components/schemas/newTask'
+ */
+//to disable security :  security: []
+// #*     security:
+//*          - bearerAuth: []
 
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: List all the tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      type: object
+ *                      properties:
+ *                          _id:
+ *                              type: string
+ *                              description: The auto-generated id of the task
+ *                              example: 1dzf5f5jbjv5555
+ *                          title:
+ *                              type: string
+ *                              description: The title of your task
+ *                              example: learn react
+ *                          duration:
+ *                              type: string
+ *                              description: The task duration
+ *                              example: 150,
+ *                          description:
+ *                              type: string
+ *                              description: The task description
+ *                              example: learn the fundamentals of react
+ *       500:
+ *         description: Some server error
+ */
+router.get("/", taskController.fetchTasks)
+// router.get("/", auth.loggedMiddleware, auth.isAdmin, taskController.fetchTasks)
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              type: object
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      required: true
+ *                      description: The task title
+ *                      example: learn react
+ *                  duration:
+ *                      type: string
+ *                      required: true
+ *                      description: The task duration
+ *                      example: 150,
+ *                  description:
+ *                      type: string
+ *                      description: The task description
+ *                      example: learn the fundamentals of react
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                  _id:
+ *                      type: string
+ *                      description: The auto-generated id of the task
+ *                      example: 1dzf5f5jbjv5555
+ *                  title:
+ *                      type: string
+ *                      description: The title of your task
+ *                      example: learn react
+ *                  duration:
+ *                      type: string
+ *                      description: The task duration
+ *                      example: 150,
+ *                  description:
+ *                      type: string
+ *                      description: The task description
+ *                      example: learn the fundamentals of react
+ *       400:
+ *         description: Bad request. You may need to verify your information.
+ *       500:
+ *         description: Some server error
+ *
+ */
+router.post("/", taskController.addTask)
+// router.post("/", auth.loggedMiddleware, taskController.addTask)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   get:
+ *     summary: get a task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                  _id:
+ *                      type: string
+ *                      description: The auto-generated id of the task
+ *                      example: 1dzf5f5jbjv5555
+ *                  title:
+ *                      type: string
+ *                      description: The title of your task
+ *                      example: learn react
+ *                  duration:
+ *                      type: string
+ *                      description: The task duration
+ *                      example: 150,
+ *                  description:
+ *                      type: string
+ *                      description: The task description
+ *                      example: learn the fundamentals of react
+ *       404:
+ *         description: Object not found
+ *       500:
+ *         description: Some server error
+ *
+ */
 router.get("/:id", auth.loggedMiddleware, taskController.getTaskById)
 
 router.patch("/:id", auth.loggedMiddleware, taskController.updateTask)
