9057ce58fc2f711838865339a943ef14535cfd2d
Yosr Naija <yosr.naija@live.com>
2023-12-13 09:25:09 +0100

    fix async/await

--- controllers/task.js
+++ controllers/task.js
@@ -1,82 +1,78 @@
 const Task = require("../models/task")
 
-const fetchTasks = (req, res) => {
-  Task.find()
-    .then((tasks) =>
-      res.status(200).json({
-        model: tasks,
-        message: "succès",
-      })
-    )
-    .catch((error) =>
-      res.status(400).json({
-        error,
-        message: "problème d'extraction",
-      })
-    )
+const fetchTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.status(200).json({ model: tasks, message: "succès" })
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+      message: "problème d'extraction",
+    })
+  }
 }
-const addTask = (req, res) => {
-  console.log(req.body)
-  delete req.body._id
-  const task = new Task({ ...req.body })
-  task
-    .save()
-    .then(() =>
-      res.status(201).json({
-        model: task,
-        message: "Objet créé !",
-      })
-    )
-    .catch((error) =>
-      res.status(400).json({
-        error: error.message,
-        message: "Données invalides",
-      })
-    )
+const addTask = async (req, res) => {
+  try {
+    const task = new Task(req.body)
+    await task.save()
+
+    res.status(201).json({
+      model: task,
+      message: "Objet créé !",
+    })
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+      message: "Données invalides",
+    })
+  }
 }
 
-const getTaskById = (req, res) => {
-  Task.findOne({ _id: req.params.id })
-    .then((task) => {
-      if (!task) {
-        res.status(404).json({
-          message: "Objet non trouvé",
-        })
-        return
-      }
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id })
+    if (!task) {
+      res.status(404).json({
+        message: "Objet non trouvé",
+      })
+    } else {
       res.status(200).json({
         model: task,
         message: "Objet trouvé",
       })
-    })
-    .catch((error) => res.status(404).json({ error: error.message }))
+    }
+  } catch (error) {
+    res.status(404).json({ error: error.message })
+  }
 }
 
-const updateTask = (req, res) => {
-  console.log(req.params.id)
-  console.log(req.body)
-  delete req.body._id
-
-  Task.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    .then((task) => {
-      if (!task) {
-        res.status(404).json({
-          message: "Objet non trouvé",
-        })
-        return
-      }
+const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+    })
+    if (!task) {
+      res.status(404).json({
+        message: "Objet non trouvé",
+      })
+    } else {
       res.status(200).json({
         model: task,
         message: "Objet modifié",
       })
-    })
-    .catch((error) => res.status(400).json({ error: error.message }))
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
-const deleteTask = (req, res) => {
-  Task.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error: error.message }))
+const deleteTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id })
+    res.status(200).json({ message: "Objet supprimé !" })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 module.exports = {
