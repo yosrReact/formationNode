188e34dc4b0e3623fa432fcdb668f3f702ba9d78
Yosr Naija <yosr.naija@live.com>
2023-12-13 09:21:25 +0100

    middleware with async/Await

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,26 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/user")
 
-module.exports.loggedMiddleware = (req, res, next) => {
+module.exports.loggedMiddleware = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]
     console.log("token: ", token)
     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET")
     const userId = decodedToken.userId
-    User.findOne({ _id: userId })
-      .then((response) => {
-        if (response) {
-          req.auth = {
-            userId: userId,
-            role: response.role,
-          }
-          next()
-        } else {
-          res.status(401).json({ error: "user doesn't exist" })
+    try {
+      const user = await User.findOne({ _id: userId })
+      if (user) {
+        req.auth = {
+          userId: userId,
+          role: user.role,
         }
-      })
-      .catch((error) => {
-        res.status(500).json({ error: error.message })
-      })
+        next()
+      } else {
+        res.status(401).json({ error: "user doesn't exist" })
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
